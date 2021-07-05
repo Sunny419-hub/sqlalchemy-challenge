59867ed37c8f4c45bbef0c777ff5ba8aaa150d95
SoniaSu <frutos.rojos17@gmail.com>
2021-07-04 22:05:47 -0500

    app

---
+++ app.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Passenger = Base.classes.passenger
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    
+    # Query all date and PRCP
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    # """Convert the query results to a dictionary using date as the key and prcp as the value"""
+   
+    all_dates = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict[date] = prcp
+        all_dates.append(prcp_dict)
+
+    return jsonify(all_dates)
+    
+   
+@app.route("/api/v1.0/passengers")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_passengers = []
+    for name, age, sex in results:
+        passenger_dict = {}
+        passenger_dict["name"] = name
+        passenger_dict["age"] = age
+        passenger_dict["sex"] = sex
+        all_passengers.append(passenger_dict)
+
+    return jsonify(all_passengers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
